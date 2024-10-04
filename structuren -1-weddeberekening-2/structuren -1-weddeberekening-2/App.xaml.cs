930a8dfc2f8b053804b5e82b6ed3bdd536e05316
MathiasRobbensPXL <Mathias.Robbens@student.pxl.be>
2024-10-04 10:07:23 +0200

    Kleur-01

---
+++ structuren -1-weddeberekening-2/structuren -1-weddeberekening-2/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace structuren__1_weddeberekening_2
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
